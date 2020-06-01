616540a92f4c01ca544867d8d1a8cadfa4e8597c
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-06-01 20:59:24 +0200

    finished mobile navigation

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,23 @@
  * @version V1.0
  * @summary This is the main JS file for this website
  */
+
+/**
+ * Navigation
+ */
+var hameburger = document.querySelector('.hamburger');
+var navigation = document.querySelector('.navigation');
+
+console.log(hameburger);
+console.log(navigation);
+
+hameburger.addEventListener('click', onHamburgerClick);
+
+function onHamburgerClick() {
+    if (navigation.classList.contains('expanded')) {
+        navigation.classList.remove('expanded');
+    }
+    else {
+        navigation.classList.add('expanded');
+    }
+}
